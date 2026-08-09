1613c98716f2b3519e62e38b55afb5ee8bd0b76d
deballap-webdev <debbieallap@gmail.com>
2026-08-09 18:16:22 +0100

    update drop down display functionality

--- build/js/domFunctions.js
+++ build/js/domFunctions.js
@@ -1,7 +1,15 @@
 export const dropDownDisplay = (event) => {
-  event.currentTarget
-    .querySelector('img[data-dropDown="true"]')
-    .classList.toggle("rotate-180");
-  event.currentTarget.nextElementSibling.classList.toggle("hidden");
-  event.currentTarget.nextElementSibling.classList.toggle("flex");
+  if (event.type === "click") {
+    event.currentTarget
+      .querySelector('img[data-dropDown="true"]')
+      .classList.toggle("rotate-180");
+    event.currentTarget.nextElementSibling.classList.toggle("hidden");
+    event.currentTarget.nextElementSibling.classList.toggle("flex");
+  } else {
+    event.currentTarget
+      .querySelector('img[data-dropDown="true"]')
+      .classList.remove("rotate-180");
+    event.currentTarget.nextElementSibling.classList.add("hidden");
+    event.currentTarget.nextElementSibling.classList.remove("flex");
+  }
 };
--- build/js/main.js
+++ build/js/main.js
@@ -4,8 +4,10 @@ const initApp = () => {
   //Add Listeners
   const unitBtn = document.getElementById("toggleUnit");
   unitBtn.addEventListener("click", dropDownDisplay);
+  unitBtn.addEventListener("focusout", dropDownDisplay);
   const dayButton = document.getElementById("chooseDay");
   dayButton.addEventListener("click", dropDownDisplay);
+  dayButton.addEventListener("focusout", dropDownDisplay);
 };
 
 document.addEventListener("DOMContentLoaded", initApp);
